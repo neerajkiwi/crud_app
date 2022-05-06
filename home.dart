e40da9ba21e78bf4171e2e364c40287ab42d0b40
neerajkiwi <100072019+neerajkiwi@users.noreply.github.com>
2022-05-06 16:28:42 +0530

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('home',style: TextStyle(fontSize: 45.0,fontWeight: FontWeight.bold),)),
+
+
+    );
+  }
+}
